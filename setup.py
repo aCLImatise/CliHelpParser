10fb893e4d35fe435211a65fde9ec2cee68203ce
Michael Milton <ttmigueltt@gmail.com>
2020-03-24 17:32:32 +1100

    Add pre-commit hooks

--- setup.py
+++ setup.py
@@ -2,28 +2,20 @@
 from setuptools import setup
 
 setup(
-    name='acclimatise',
-    packages=['acclimatise'],
-    version='0.0.1',
+    name="acclimatise",
+    packages=["acclimatise"],
+    version="0.0.1",
     install_requires=[
-        'pyparsing',
-        'jinja2',
-        'spacy',
-        'cwlgen',
-        'dataclasses',
-        'miniwdl',
-        'wordsegment',
-        'inflection',
-        'illusional.wdlgen~=0.2'
+        "pyparsing",
+        "jinja2",
+        "spacy",
+        "cwlgen",
+        "dataclasses",
+        "miniwdl",
+        "wordsegment",
+        "inflection",
+        "illusional.wdlgen~=0.2",
     ],
-    entry_points={
-        'console_scripts': [
-            'acclimatise = acclimatise.cli:main'
-        ]
-    },
-    extras_require={
-        'dev': [
-            'pytest'
-        ],
-    }
+    entry_points={"console_scripts": ["acclimatise = acclimatise.cli:main"]},
+    extras_require={"dev": ["pytest", "pre-commit"],},
 )
