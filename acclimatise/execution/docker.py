654e272114bfc020f951a2430e16522909fd6ec6
Michael Milton <ttmigueltt@gmail.com>
2020-08-20 17:42:35 +1000

    Manual generator

--- acclimatise/execution/docker.py
+++ acclimatise/execution/docker.py
@@ -13,14 +13,16 @@ class DockerExecutor(Executor):
     def __init__(self, container: "docker.models.containers.Container"):
         self.container = container
 
+    @staticmethod
+    def frames(socket):
+        for frame in frames_iter(socket, tty=False):
+            yield demux_adaptor(*frame)
+
     def execute(self, command: List[str]) -> str:
         _, socket = self.container.exec_run(
             command, stdout=True, stderr=True, demux=True, socket=True
         )
 
         socket._sock.settimeout(5)
-        gen = frames_iter(socket, tty=False)
-        gen = (demux_adaptor(*frame) for frame in gen)
-
-        stdout, stderr = consume_socket_output(gen, demux=True)
+        stdout, stderr = consume_socket_output(self.frames(socket), demux=True)
         return (stdout or stderr).decode()
