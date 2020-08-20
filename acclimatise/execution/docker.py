a7eaf9d8460cbe67126b4b7d7bd958e6e6f47e47
Michael Milton <ttmigueltt@gmail.com>
2020-08-20 14:25:40 +0000

    Timeout working

--- acclimatise/execution/docker.py
+++ acclimatise/execution/docker.py
@@ -2,8 +2,22 @@ from typing import List
 
 from docker.utils.socket import consume_socket_output, demux_adaptor, frames_iter
 
+
+import errno
+import os
+from select import select as original_select
+import select
+import socket as pysocket
+import struct
+
+import six
+
 from . import Executor
 
+from unittest.mock import patch
+
+def timeout_select(rlist, wlist, xlist):
+    return original_select(rlist, wlist, xlist, 5)
 
 class DockerExecutor(Executor):
     """
@@ -15,14 +29,15 @@ class DockerExecutor(Executor):
 
     @staticmethod
     def frames(socket):
-        for frame in frames_iter(socket, tty=False):
-            yield demux_adaptor(*frame)
+        with patch.object(select, 'select', new=timeout_select):
+            for frame in frames_iter(socket, tty=False):
+                yield demux_adaptor(*frame)
 
     def execute(self, command: List[str]) -> str:
         _, socket = self.container.exec_run(
             command, stdout=True, stderr=True, demux=True, socket=True
         )
-
         socket._sock.settimeout(5)
+
         stdout, stderr = consume_socket_output(self.frames(socket), demux=True)
         return (stdout or stderr).decode()
