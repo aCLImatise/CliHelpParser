35b381ef648803c81e10a9859994c8c68b8d1322
Michael Milton <ttmigueltt@gmail.com>
2020-08-22 06:09:04 +0000

    Simplify patching

--- acclimatise/execution/docker.py
+++ acclimatise/execution/docker.py
@@ -27,17 +27,13 @@ class DockerExecutor(Executor):
     def __init__(self, container: "docker.models.containers.Container"):
         self.container = container
 
-    @staticmethod
-    def frames(socket):
-        with patch.object(select, 'select', new=timeout_select):
-            for frame in frames_iter(socket, tty=False):
-                yield demux_adaptor(*frame)
-
     def execute(self, command: List[str]) -> str:
         _, socket = self.container.exec_run(
             command, stdout=True, stderr=True, demux=True, socket=True
         )
         socket._sock.settimeout(5)
 
-        stdout, stderr = consume_socket_output(self.frames(socket), demux=True)
+        with patch.object(select, 'select', new=timeout_select):
+            gen = (demux_adaptor(*frame) for frame in frames_iter(socket, tty=False))
+            stdout, stderr = consume_socket_output(gen, demux=True)
         return (stdout or stderr).decode()
